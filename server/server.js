c6b3174bba55b3cbc76fe57fbd94e9898558b554
Zacariah Regalado <zgr6@nau.edu>
2026-08-11 17:16:27 -0700

    connect React frontend to Express backend

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+
 
 const app = express();
 const PORT = 5001;
+
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Antler API is running");
